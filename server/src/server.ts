e61ed2b785fec9d81dd4fa0780be138960cbeb37
lvdias98 <lvdias98@gmail.com>
2022-05-08 01:22:14 -0300

    Add new port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(routes);
 
-const port = 3333;
+const port = process.env.PORT ?? 3333;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
