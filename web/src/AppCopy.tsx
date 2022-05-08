3c7db45ea0ceb88a628ad2ccc14b79dc92e9a431
lvdias98 <lvdias98@gmail.com>
2022-05-08 00:51:49 -0300

    First commit web

---
+++ web/src/AppCopy.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props:ButtonProps){
+  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="OK" />
+      <Button />
+    </div>
+  )
+}
+
+export default App;
---
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,85 @@
+import { FormEvent, useState } from "react";
+
+import { api } from "../../../lib/api";
+
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { Loading } from "../../Loading";
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState<string>('');
+  const [isSendingFeedback,setIsSendingFeedback] = useState<boolean>(false);
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  async function handleSubmitFeedback(event:FormEvent) {
+    event.preventDefault();
+    setIsSendingFeedback(true);
+    
+    await api.post('feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
+    });
+    
+    setIsSendingFeedback(false);
+    onFeedbackSent();
+  }
+
+  return (
+    <>
+      <header>
+        <button 
+          type="button" 
+          onClick={onFeedbackRestartRequested}
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
+          {feedbackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+
+
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+        <textarea
+          onChange={event => setComment(event.target.value)}
+          placeholder="Conte com detalhes o que está acontecendo..."
+          className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:outline-none focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none strollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton onScreenshotTook={setScreenshot} screenshot={screenshot!} />
+
+          <button
+            type="submit"
+            disabled={comment.length === 0 || isSendingFeedback }
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+          >
+            {
+              isSendingFeedback
+              ?
+              <Loading/>
+              :
+              <>Enviar feedback</>
+            }
+          </button>
+        </footer>
+      </form>
+    </>
+  )
+}
